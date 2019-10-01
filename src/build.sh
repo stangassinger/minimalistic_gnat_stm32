283009e7fc659b959ec7c5f9d7bc81958684f27a
guenther <guenther.stangassinger@gmx.de>
2019-10-01 12:40:01 +0000

    adding doc

--- src/build.sh
+++ src/build.sh
@@ -3,3 +3,4 @@ arm-linux-gnueabi-gcc -c -gnatp -mcpu=cortex-m4 -mthumb -mfloat-abi=soft program
 arm-linux-gnueabi-gcc -c -gnatp -mcpu=cortex-m4 -mthumb -mfloat-abi=soft utils.adb
 arm-linux-gnueabi-ld -T flash.ld -o program.elf  pins.o program.o utils.o
 arm-linux-gnueabi-objcopy -O binary program.elf program.bin
+rm -f *.ali *.o *.elf
